8b242f397735af762bdf4216bd83bc2c15862714
본인tosknm5 <tosknm5@gmail.com>
2025-07-10 18:26:58 +0900

    20250710

--- src/day08/Rectangle.java
+++ src/day08/Rectangle.java
@@ -1,14 +1,24 @@
 package day08;
 
-
 public class Rectangle {
-    // 멤버 변수
-    public int width;
-    public int height;
-
-
-    // 넓이 계산 메서드
-    public int getArea() {
-        return width * height;
+    // 멤버변수
+    int width;
+    int height;
+    // 생성자 : width와 height를 매개변수로 받아
+    Rectangle( int width , int height ){
+        // 초기화하는 생성자
+        this.width = width;
+        this.height = height;
     }
-}
+    // 메소드 :
+    // 1. 메소드명 : getArea
+    // 2. 매개변수 : x
+    // 3. 반환값 : 곱하기계산결과 ,   int result;
+    int getArea( ){
+        // 멤버변수( getArea() 메소드를 호출한 객체내 ) 끼리 계산
+        int result = width * height;
+        // 결과 반환
+        return result;
+    } // func end
+
+} // class end
